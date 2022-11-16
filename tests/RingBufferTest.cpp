693d239c4c6aef5989cfaff3d24d1c1b078929dc
memleaker <memleak@126.com>
2022-11-16 20:28:41 +0800

    修改测试用例，加入多线程下测试

--- tests/RingBufferTest.cpp
+++ tests/RingBufferTest.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <thread>
+
+#include <unistd.h>
 
 #include <RingBuffer.h>
+
+void TestThread1(trbuf::RingBuffer &buf)
+{
+	char str[16];
+
+	for (auto i = 0; i < 10000; i++)
+	{
+		buf.CopyToRing("Thread1", 8);
+		buf.MoveFromRing(str, 8);
+	}
+
+	sleep(1);
+
+	buf.CopyToRing("Thread1", 8);
+}
+
+void TestThread2(trbuf::RingBuffer &buf)
+{
+	char str[16];
+
+	for (auto i = 0; i < 10000; i++)
+	{
+		buf.CopyToRing("Thread2", 8);
+		buf.MoveFromRing(str, 8);
+	}
+
+	sleep(1);
+
+	buf.CopyToRing("Thread2", 8);
+}
 
 int main(void)
 {
@@ -9,6 +42,8 @@ int main(void)
 	char str4[sizeof(str3)] = {0};
 	char str5[48] = "12345612345612345612345612345612345612345612345";
 	char str6[48];
+	char str7[8];
+	char str8[8];
 	trbuf::RingBuffer buf(63);
 
 	std::cout << "-----------------------------------" << std::endl;
@@ -95,7 +130,31 @@ int main(void)
 	std::cout << "Find '2'" << buf.FindChOffset('2') << std::endl;
 	std::cout << "-----------------------------------" << std::endl;
 
-	std::cout << "Create 2 Thread: Producer Consumer" << std::endl;
+	std::cout << "-----------------------------------" << std::endl;
+	std::cout << "Create 2 Thread: Producer And Consumer" << std::endl;
+	std::thread th1(TestThread1, std::ref(buf));
+	std::thread th2(TestThread2, std::ref(buf));
+	th1.join();
+	th2.join();
+	std::cout << "Thread Destory" << std::endl;
+	std::cout << "Cap: " << buf.Capacity() << std::endl;
+	std::cout << "Used: " << buf.UsedSize()	<< std::endl;
+	std::cout << "Free: " << buf.FreeSize()	<< std::endl;
+	std::cout << "Isempty: " << buf.IsEmpty()	<< std::endl;
+	std::cout << "IsFull: " << buf.IsFull()	<< std::endl;
+	std::cout << "-----------------------------------" << std::endl;
+
+	std::cout << "-----------------------------------" << std::endl;
+	buf.MoveFromRing(str7, 8);
+	std::cout << "Move From Buf: " << str7 << std::endl;
+	buf.MoveFromRing(str8, 8);
+	std::cout << "Move From Buf: " << str8 << std::endl;
+	std::cout << "Cap: " << buf.Capacity() << std::endl;
+	std::cout << "Used: " << buf.UsedSize()	<< std::endl;
+	std::cout << "Free: " << buf.FreeSize()	<< std::endl;
+	std::cout << "Isempty: " << buf.IsEmpty()	<< std::endl;
+	std::cout << "IsFull: " << buf.IsFull()	<< std::endl;
+	std::cout << "-----------------------------------" << std::endl;
 
 	return 0;
 }
